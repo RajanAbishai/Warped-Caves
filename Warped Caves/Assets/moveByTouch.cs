168e4a52d3eb0e967029c43cf8da3849361e454d
Rajan <themailboxofabi@yahoo.com>
2023-01-11 14:04:10 +0530

    added TouchAndGo
    TouchAndGo script only works on mobile.

---
+++ Warped Caves/Assets/moveByTouch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveByTouch : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if (Input.touchCount > 0) // are there any touches at all
+        {
+            print("detected");
+
+            Touch touch = Input.GetTouch(0); //0, 1, 2
+
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            touchPosition.z = 0f;
+            transform.position = touchPosition;
+
+        }
+
+       // else if (Input.touchCount == 0) { print("no touch"); }
+    }
+
+
+
+    public void touchinput()
+    {
+        print("touched");
+    }
+
+}
