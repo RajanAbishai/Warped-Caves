df8d04aecef348beb9b432dfb435028a19a0eee1
Rajan <themailboxofabi@yahoo.com>
2023-01-14 09:43:42 +0530

    other movement script added

---
+++ Warped Caves/Assets/playerWalk.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerWalk : MonoBehaviour
+{
+
+    //private playerAnimation playerAnim;
+    private Rigidbody2D myBody;
+
+    private float speed = 2f;
+
+    void Start()
+    {
+        //playerAnim() = GetComponent<PlayerAnimation>();
+        myBody = GetComponent<Rigidbody2D>();
+
+        
+    }
+
+    
+    void Update()
+    {
+        //DetectInput(); disabled to prevent the character from moving using keyboard controls
+
+    }
+
+    public void MoveUp()
+    {
+        //myBody.velocity = new Vector2(speed, myBody.velocity.x);
+        myBody.velocity = new Vector2(myBody.velocity.x, speed); //modified
+
+    }
+
+    public void MoveDown()
+    {
+        myBody.velocity = new Vector2(myBody.velocity.x, -speed); //modified
+    }
+
+
+    public void MoveLeft()
+    {
+        myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+
+    }
+
+
+    public void MoveRight()
+    {
+        myBody.velocity = new Vector2(speed, myBody.velocity.y);
+    }
+
+    public void StopMoving()
+    {
+        myBody.velocity = new Vector2(0f, myBody.velocity.y);
+    }
+
+    void DetectInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+
+        if (x > 0)
+        {
+            MoveRight();
+        }
+
+        else if (x < 0)
+        {
+            MoveLeft();
+        }
+
+        else
+        {
+            StopMoving();
+        }
+
+
+    }
+
+
+
+
+}
---
+++ Warped Caves/Assets/touchControls.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchControls : MonoBehaviour
+{
+
+    private playerWalk pw;
+
+    void Start()
+    {
+        pw = GetComponent<playerWalk>();
+
+    }
+
+    
+    void Update()
+    {
+
+        if (Input.touchCount > 0) // we can have upto 5 touches simultaneously on the screen and it will detect all of them
+        {
+            Touch t = Input.GetTouch(0); // Index is mentioned because this will be the first touch
+            // t.position is in screen coordinates because we are touching the phone.. but our character is in world coordinates
+
+            Vector3 t_Pos = Camera.main.ScreenToWorldPoint(t.position);
+
+
+            if (t_Pos.x > 0 && t_Pos.y>0)
+            {
+                print("Right and up"); //YES
+                pw.MoveRight();
+                pw.MoveUp(); // move up added
+            }
+
+
+            else if (t_Pos.x < 0 && t_Pos.y<0)
+            {
+                print("Left and Down "); //YES
+                pw.MoveLeft();
+                pw.MoveDown(); // move down added
+            }
+
+
+            else if (t_Pos.x < 0 && t_Pos.y > 0)
+            {
+                print("Left and up"); //YES
+                pw.MoveLeft();
+                pw.MoveUp(); //move up added
+            }
+
+            else if (t_Pos.x > 0 && t_Pos.y < 0)
+            {
+                print("left and down"); //YES
+                pw.MoveRight();
+                pw.MoveDown();
+
+            }
+
+
+
+
+            else { pw.StopMoving(); }
+
+
+        }
+        
+    }
+
+
+
+
+
+}
